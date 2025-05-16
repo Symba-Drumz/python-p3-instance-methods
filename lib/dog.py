c7b6cdb5f910dea2a143310401193593511d3b33
alfazachmmosi@TimeMachine <zalfa3890@gmail.com>
2025-05-16 07:12:14 +0300

    complete class and instance lab by writing the required code

--- lib/dog.py
+++ lib/dog.py
@@ -2,6 +2,15 @@
 
 class Dog:
     # Class body goes here
+    def bark(self): 
+        print("Woof!") #Instance method definition
+    def sit(self):
+        print("The dog is sitting.")    
+    
+fido = Dog() # Create an instance of the Dog class
+fido.bark() # Call the bark method on the instance
+fido.sit() # Call the sit method on the instance
 
-    #Instance method definition
-    pass
+snoopy = Dog() # Create another instance of the Dog class
+snoopy.bark() # Call the bark method on the second instance
+snoopy.sit() # Call the sit method on the second instance
--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
 class Person:
-    # Class body goes here
+   def talk(self):
+      print("Hello World!") #Instance method definition
+    
+   def walk(self):
+        print("The person is walking.")
 
-    #Instance method definition
-    pass
+greeting = Person() # Create an instance of the Person class
+greeting.talk() # Call the talk method on the instance
+walking = Person() # Create another instance of the Person class
+walking.walk() # Call the walk method on the second instance          
